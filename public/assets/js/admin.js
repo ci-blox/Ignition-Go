8762f3d461482e8833dab282a704fdbb7e35cf50
Bob L <Bob L>
2016-02-05 12:31:42 -0600

    Add public initial, including api assets

---
+++ public/assets/js/admin.js
@@ -0,0 +1,13 @@
+/**
+ * Scripts for Admin Panel
+ */
+
+$(document).ready(function() {
+	console.log("Admin Panel");
+	
+	// Sortable
+	var el = $('.sortable');
+	for (var i=0; i<el.length; i++) {
+		var sortable = Sortable.create(el[i]);
+	}
+});
