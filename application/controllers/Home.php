86f02db78d72f02196aea78f2a21154b13844fcc
Bob L <Bob L>
2016-02-05 13:56:07 -0600

    User model stub, home view

---
+++ application/controllers/Home.php
@@ -0,0 +1,73 @@
+<?php defined('BASEPATH') || exit('No direct script access allowed');
+
+/**
+ * Home controller
+ *
+ * The base controller which displays the homepage.
+ *
+ */
+class Home extends MX_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->helper('application');
+		$this->load->library('Template');
+		$this->load->library('Assets');
+		$this->lang->load('application');
+		$this->load->library('events');
+
+        // Make the requested page var available
+        $this->requested_page = isset($_SESSION['requested_page']) ? $_SESSION['requested_page'] : null;
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Displays the homepage
+	 *
+	 * @return void
+	 */
+	public function index()
+	{
+		$this->load->library('users/auth');
+		$this->set_current_user();
+		Template::render();
+	}//end index()
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * If the Auth lib is loaded, it will set the current user, since users
+	 * will never be needed if the Auth library is not loaded. By not requiring
+	 * this to be executed and loaded for every command, we can speed up calls
+	 * that don't need users at all, or rely on a different type of auth, like
+	 * an API or cronjob.
+	 *
+	 * Copied from Base_Controller
+	 */
+	protected function set_current_user()
+	{
+        if (class_exists('Auth')) {
+			// Load our current logged in user for convenience
+            if ($this->auth->is_logged_in()) {
+				$this->current_user = clone $this->auth->user();
+
+				// if the user has a language setting then use it
+                if (isset($this->current_user->language)) {
+					$this->config->set_item('language', $this->current_user->language);
+				}
+            } else {
+				$this->current_user = null;
+			}
+
+			// Make the current user available in the views
+            if (! class_exists('Template')) {
+				$this->load->library('Template');
+			}
+			Template::set('current_user', $this->current_user);
+		}
+	}
+}
+/* end ./application/controllers/Home.php */
---
+++ application/views/home/index.php
@@ -0,0 +1,37 @@
+<div class="row" text-align="center">
+	<h1>Welcome to Ignition Go!</h1>
+
+	<p class="lead">Kickstart your CodeIgniter applications with Blox!</p>
+</div>
+
+<div class="row">
+Note: STILL WIP- - - not ready yet!!!<br>    
+    &nbsp;<br>
+<h3>Helpful resources:</h3>
+</div>
+<div class="row">
+
+	<div class="col-sm-6">
+		<h4>CodeIgniter</h4>
+
+		<p><a href="http://ellislab.com/codeigniter" target="_blank">CodeIgniter <?php echo CI_VERSION; ?></a> is a proven PHP framework. In order to make the best use of it, you should be comfortable with CodeIgniter and its <a href="http://ellislab.com/codeigniter/user-guide/" target="_blank">documentation</a> first.</p>
+
+	</div>
+
+	<div class="col-sm-6">
+		<h4>Bootstrap/JQuery</h4>
+
+		<p>Interface is <a href="http://bootstrp.com">Bootstrap</a> UI framework and <a href="http://jquery.com/">jQuery</a> as the Javascript engine.</p>
+
+	</div>
+</div>
+
+<div class="row">
+
+	<div class="col-sm-6">
+		<h4>Other</h4>
+
+		<p>A <a href="https://bitbucket.org/wiredesignz/codeigniter-modular-extensions-hmvc">modular system</a> that allows code re-use, and overriding core modules with custom modules.</p>
+	</div>
+
+</div>
