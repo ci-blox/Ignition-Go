4a0829c82aa18eb0a62e0059d75580b785384206
boblennes <boblennes@msn.com>
2016-03-12 10:18:13 -0600

    clear out base_url value

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://igo.local/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
