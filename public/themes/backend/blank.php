2997733ceb697dbee59379fb3d3fada3b9ee4b58
Bob L <Bob L>
2016-09-17 16:33:47 -0500

    add missing css and fix jquery ref in backend

--- public/themes/backend/blank.php
+++ public/themes/backend/blank.php
@@ -33,9 +33,9 @@ Apply one or more of the following
 
 <!-- REQUIRED JS SCRIPTS -->
 
-<!-- jQuery 2.1.4 -->
-<script src="<?php echo $adm_url; ?>plugins/jQuery/jQuery-2.1.4.min.js"></script>
-<!-- Bootstrap 3.3.5 -->
+<!-- jQuery -->
+<script src="<?php echo base_url(); ?>components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.x -->
 <script src="<?php echo $adm_url; ?>bootstrap/js/bootstrap.min.js"></script>
 <!-- iCheck -->
 <script src="<?php echo $adm_url; ?>plugins/iCheck/icheck.min.js"></script>
--- public/themes/backend/index.php
+++ public/themes/backend/index.php
@@ -156,9 +156,9 @@ Apply one or more of the following
 
 <!-- REQUIRED JS SCRIPTS -->
 
-<!-- jQuery 2.2.0 -->
-<script src="<?php echo $adm_url; ?>plugins/jQuery/jQuery-2.2.0.min.js"></script>
-<!-- Bootstrap 3.3.5 -->
+<!-- jQuery -->
+<script src="<?php echo base_url(); ?>components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.x -->
 <script src="<?php echo $adm_url; ?>bootstrap/js/bootstrap.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo $adm_url; ?>dist/js/app.min.js"></script>
