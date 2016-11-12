f3d31a74ec99b2ee1d7fa61705a1cb773c13c073
Bob L <Bob L>
2016-11-12 07:00:48 -0600

    Use gulp uglify + rename  now

--- gulpfile.js/tasks/uglify.js
+++ gulpfile.js/tasks/uglify.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglifyjs'),
+	uglify = require('gulp-uglify'),
 	handleErrors = require('../lib/handleErrors'),
 	config = require('../config').js;
 
 gulp.task('uglify:frontend', function() {
 	return gulp.src(config.src.frontend)
 		.pipe(uglify(config.dest_file.frontend, config.settings))
+        .pipe(rename({
+               extname: '.min.js'
+        }))
 		.on('error', handleErrors)
 		.pipe(gulp.dest(config.dest));
 });
@@ -13,6 +16,9 @@ gulp.task('uglify:frontend', function() {
 gulp.task('uglify:admin', function() {
 	return gulp.src(config.src.admin)
 		.pipe(uglify(config.dest_file.admin, config.settings))
+        .pipe(rename({
+               extname: '.min.js'
+        }))
 		.on('error', handleErrors)
 		.pipe(gulp.dest(config.dest));
 });
