a6b33bc63041d7b0577ab1cf9c678b23137dfdd9
Bob L <Bob L>
2016-11-17 12:11:15 -0600

    CI 3.1.2 index.php updated

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
  *
  * This content is released under the MIT License (MIT)
  *
- * Copyright (c) 2014 - 2015, British Columbia Institute of Technology
+ * Copyright (c) 2014 - 2016, British Columbia Institute of Technology
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,20 +26,20 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- * @package CodeIgniter
- * @author  EllisLab Dev Team
- * @copyright   Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
- * @copyright   Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
- * @license http://opensource.org/licenses/MIT  MIT License
- * @link    http://codeigniter.com
- * @since   Version 1.0.0
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2016, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ * @since	Version 1.0.0
  * @filesource
  */
 
 /*
- *------------------------------------------------------------------------------
+ *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
- *------------------------------------------------------------------------------
+ *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
  * current environment. Setting the environment also influences
@@ -52,36 +52,62 @@
  *     production
  *
  * NOTE: If you change these, also change the error_reporting() code below
- */
-define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+ */	
+define('DEFAULT_ENV', 'development');
+
+// the below if logic detects and kicks in if called from command line (cli),
+// so for cli we have the ability to pass in --env option
+if ((php_sapi_name() == 'cli') or defined('STDIN'))
+{
+	$environment = DEFAULT_ENV;
+	if (isset($argv)) 
+	{
+		// grab the --env argument, and the one that comes next
+
+		$key = (array_search('--env', $argv));
+		$environment = $argv[$key +1];
+
+		// get rid of them so they don't get passed in to our method as parameter values
+
+		unset($argv[$key], $argv[$key +1]);
+	}  
+  	define('ENVIRONMENT', $environment);
+} 
+
+if (!defined('ENVIRONMENT')) define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : DEFAULT_ENV);
 
 /*
- *------------------------------------------------------------------------------
+ *---------------------------------------------------------------
  * ERROR REPORTING
- *------------------------------------------------------------------------------
+ *---------------------------------------------------------------
  *
- * Different environments will require different levels of error reporting. By
- * default, development will show errors but testing and production will hide
- * them.
+ * Different environments will require different levels of error reporting.
+ * By default development will show errors but testing and live will hide them.
  */
-switch (ENVIRONMENT) {
-    case 'development':
-        error_reporting(-1);
-        ini_set('display_errors', 1);
-        break;
-    case 'testing':
-    case 'production':
-        ini_set('display_errors', 0);
-        if (version_compare(PHP_VERSION, '5.3', '>=')) {
-            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-        } else {
-            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-        }
-        break;
-    default:
-        header('HTTP/1.1 503 Service Unavailable.', true, 503);
-        echo 'The application environment is not set correctly.';
-        exit(1); // EXIT_ERROR
+switch (ENVIRONMENT)
+{
+	case 'development':
+		error_reporting(-1);
+		ini_set('display_errors', 1);
+	break;
+
+	case 'testing':
+	case 'production':
+		ini_set('display_errors', 0);
+		if (version_compare(PHP_VERSION, '5.3', '>='))
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+		}
+		else
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+		}
+	break;
+
+	default:
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'The application environment is not set correctly.';
+		exit(1); // EXIT_ERROR
 }
 
 /*
@@ -113,59 +139,62 @@ if (ini_get('date.timezone') == ''
 
 /*
  *------------------------------------------------------------------------------
- * Ignition Go core path
+ * Ignition Go Application PATH
  *------------------------------------------------------------------------------
  *
  * Change the first "path" variable, and the individual paths will be set accordingly.
  */
 $path = "..";
-$igo_path = "{$path}/igocore";
 
 /*
- *------------------------------------------------------------------------------
- * SYSTEM FOLDER NAME
- *------------------------------------------------------------------------------
+ *---------------------------------------------------------------
+ * SYSTEM DIRECTORY NAME
+ *---------------------------------------------------------------
  *
- * This variable must contain the name of your "system" folder.
- * Include the path if the folder is not in the same directory
- * as this file.
+ * these variables must contain the name of your Ignition Go core
+ * and CodeIgniter "system" directory.
+ * Set the path if it is not in the same directory as this file.
  */
-$system_path = "{$path}/igocore/system";
+    $igocore_path = $path.DIRECTORY_SEPARATOR."igocore";
+	$system_path = $igocore_path.DIRECTORY_SEPARATOR.'system';
 
 /*
- *------------------------------------------------------------------------------
- * APPLICATION FOLDER NAME
- *------------------------------------------------------------------------------
+ *---------------------------------------------------------------
+ * APPLICATION DIRECTORY NAME
+ *---------------------------------------------------------------
  *
  * If you want this front controller to use a different "application"
- * folder than the default one you can set its name here. The folder
- * can also be renamed or relocated anywhere on your server. If
- * you do, use a full server path. For more info please see the user guide:
- * http://codeigniter.com/user_guide/general/managing_apps.html
+ * directory than the default one you can set its name here. The directory
+ * can also be renamed or relocated anywhere on your server. If you do,
+ * use an absolute (full) server path.
+ * For more info please see the user guide:
+ *
+ * https://codeigniter.com/user_guide/general/managing_apps.html
  *
  * NO TRAILING SLASH!
  */
-$application_folder = "{$path}/application";
+	$application_folder =  $path.DIRECTORY_SEPARATOR.'application';
 
 /*
- *------------------------------------------------------------------------------
- * VIEW FOLDER NAME
- *------------------------------------------------------------------------------
+ *---------------------------------------------------------------
+ * VIEW DIRECTORY NAME
+ *---------------------------------------------------------------
  *
- * If you want to move the view folder out of the application
- * folder set the path to the folder here. The folder can be renamed
+ * If you want to move the view directory out of the application
+ * directory, set the path to it here. The directory can be renamed
  * and relocated anywhere on your server. If blank, it will default
- * to the standard location inside your application folder. If you
- * do move this, use the full server path to this folder.
+ * to the standard location inside your application directory.
+ * If you do move this, use an absolute (full) server path.
  *
  * NO TRAILING SLASH!
  */
-$view_folder = '';
+	$view_folder = '';
+
 
 /*
- *------------------------------------------------------------------------------
+ * --------------------------------------------------------------------
  * DEFAULT CONTROLLER
- *------------------------------------------------------------------------------
+ * --------------------------------------------------------------------
  *
  * Normally you will set your default controller in the routes.php file.
  * You can, however, force a custom routing by hard-coding a
@@ -181,20 +210,21 @@ $view_folder = '';
  *
  * Un-comment the $routing array below to use this feature
  */
-// The directory name, relative to the "controllers" folder. Leave blank if the
-// controller is not in a sub-folder within the "controllers" folder
-// $routing['directory'] = '';
+	// The directory name, relative to the "controllers" directory.  Leave blank
+	// if your controller is not in a sub-directory within the "controllers" one
+	// $routing['directory'] = '';
 
-// The controller class file name. Example:  Mycontroller.php
-// $routing['controller'] = '';
+	// The controller class file name.  Example:  mycontroller
+	// $routing['controller'] = '';
 
-// The controller function to be called.
-// $routing['function'] = '';
+	// The controller function you wish to be called.
+	// $routing['function']	= '';
+
 
 /*
- *------------------------------------------------------------------------------
+ * -------------------------------------------------------------------
  *  CUSTOM CONFIG VALUES
- *------------------------------------------------------------------------------
+ * -------------------------------------------------------------------
  *
  * The $assign_to_config array below will be passed dynamically to the
  * config class when initialized. This allows you to set custom config
@@ -205,107 +235,159 @@ $view_folder = '';
  *
  * Un-comment the $assign_to_config array below to use this feature
  */
-// $assign_to_config['name_of_config_item'] = 'value of config item';
+	// $assign_to_config['name_of_config_item'] = 'value of config item';
 
-// -----------------------------------------------------------------------------
-// END OF USER CONFIGURABLE SETTINGS. DO NOT EDIT BELOW THIS LINE
-// -----------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------
+// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
+// --------------------------------------------------------------------
 
 /*
- * -----------------------------------------------------------------------------
- *  Resolve the system path for increased reliability.
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------
+ *  Resolve the system path for increased reliability
+ * ---------------------------------------------------------------
  */
 
-// Set the current directory correctly for CLI requests.
-if (defined('STDIN')) {
-    chdir(dirname(__FILE__));
-}
+	// Set the current directory correctly for CLI requests
+	if (defined('STDIN'))
+	{
+		chdir(dirname(__FILE__));
+	}
 
-if (($_temp = realpath($system_path)) !== false) {
-    $system_path = $_temp.'/';
-} else {
-    // Ensure there's a trailing slash
-    $system_path = rtrim($system_path, '/').'/';
-}
+	if (($_temp = realpath($system_path)) !== FALSE)
+	{
+		$system_path = $_temp.DIRECTORY_SEPARATOR;
+	}
+	else
+	{
+		// Ensure there's a trailing slash
+		$system_path = strtr(
+			rtrim($system_path, '/\\'),
+			'/\\',
+			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+		).DIRECTORY_SEPARATOR;
+	}
 
-// Is the system path correct?
-if (! is_dir($system_path)) {
-    header('HTTP/1.1 503 Service Unavailable.', true, 503);
-    echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '
-        . pathinfo(__FILE__, PATHINFO_BASENAME);
-    exit(3); // EXIT_CONFIG
-}
+	// Repeat for IGOCORE
+        echo $igocore_path;
+	if (($_temp = realpath($igocore_path)) !== FALSE)
+	{
+		$igocore_path = $_temp.DIRECTORY_SEPARATOR;
+	}
+	else
+	{
+		// Ensure there's a trailing slash
+		$igocore_path = strtr(
+			rtrim($igocore_path, '/\\'),
+			'/\\',
+			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+		).DIRECTORY_SEPARATOR;
+	}
+
+	// Is the system path correct?
+	if ( ! is_dir($system_path) ||  ! is_dir($igocore_path))
+	{
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'Your system folder paths do not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
+		exit(3); // EXIT_CONFIG
+	}
 
 /*
- * -----------------------------------------------------------------------------
- *  Now that the path has been validated, set the main path constants.
- * -----------------------------------------------------------------------------
+ * -------------------------------------------------------------------
+ *  Now that we know the path, set the main path constants
+ * -------------------------------------------------------------------
  */
-// The name of THIS file.
-define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+	// The name of THIS file
+	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-// The PHP file extension.
-// @deprecated since CI 2.0.3.
-//
-// This constant is not included in CI 3...
-define('EXT', '.php');
+	// Path to the system directory
+	define('BASEPATH', $system_path);
 
-// Path to the system folder.
-define('BASEPATH', str_replace('\\', '/', $system_path));
+	// Path to the front controller (this file) directory
+	define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
-// Path to the front controller (this file).
-define('FCPATH', dirname(__FILE__) . '/');
+    // The path to the Ignition Go core folder.
+    define('IGOPATH', $igocore_path.DIRECTORY_SEPARATOR);
 
-// Name of the "system folder".
-define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+	// Name of the "system" directory
+	define('SYSDIR', basename(BASEPATH));
 
-// The path to the Ignition Go folder.
-define('IGOPATH', $igo_path . '/');
+	// The path to the "application" directory
+	if (is_dir($application_folder))
+	{
+		if (($_temp = realpath($application_folder)) !== FALSE)
+		{
+			$application_folder = $_temp;
+		}
+		else
+		{
+			$application_folder = strtr(
+				rtrim($application_folder, '/\\'),
+				'/\\',
+				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+			);
+		}
+	}
+	elseif (is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
+	{
+		$application_folder = BASEPATH.strtr(
+			trim($application_folder, '/\\'),
+			'/\\',
+			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+		);
+	}
+	else
+	{
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+		exit(3); // EXIT_CONFIG
+	}
 
-// The path to the "application" folder
-if (is_dir($application_folder)) {
-    if (($_temp = realpath($application_folder)) !== false) {
-        $application_folder = $_temp;
-    }
+	define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 
-    define('APPPATH', $application_folder . DIRECTORY_SEPARATOR);
-} else {
-    if (! is_dir(BASEPATH . $application_folder . DIRECTORY_SEPARATOR)) {
-        header('HTTP/1.1 503 Service Unavailable.', true, 503);
-        echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '
-            . SELF;
-        exit(3); // EXIT_CONFIG
-    }
+	// The path to the "views" directory
+	if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
+	{
+		$view_folder = APPPATH.'views';
+	}
+	elseif (is_dir($view_folder))
+	{
+		if (($_temp = realpath($view_folder)) !== FALSE)
+		{
+			$view_folder = $_temp;
+		}
+		else
+		{
+			$view_folder = strtr(
+				rtrim($view_folder, '/\\'),
+				'/\\',
+				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+			);
+		}
+	}
+	elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
+	{
+		$view_folder = APPPATH.strtr(
+			trim($view_folder, '/\\'),
+			'/\\',
+			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+		);
+	}
+	else
+	{
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+		exit(3); // EXIT_CONFIG
+	}
 
-    define('APPPATH', BASEPATH . $application_folder . DIRECTORY_SEPARATOR);
-}
-
-// The path to the "views" folder
-if (! is_dir($view_folder)) {
-    if (! empty($view_folder) && is_dir(APPPATH . $view_folder . DIRECTORY_SEPARATOR)) {
-        $view_folder = APPPATH . $view_folder;
-    } elseif (! is_dir(APPPATH . 'views' . DIRECTORY_SEPARATOR)) {
-        header('HTTP/1.1 503 Service Unavailable.', true, 503);
-        echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '
-            . SELF;
-        exit(3); // EXIT_CONFIG
-    } else {
-        $view_folder = APPPATH . 'views';
-    }
-}
-
-if (($_temp = realpath($view_folder)) !== false) {
-    $view_folder = $_temp . DIRECTORY_SEPARATOR;
-} else {
-    $view_folder = rtrim($view_folder, '/\\') . DIRECTORY_SEPARATOR;
-}
-
-define('VIEWPATH', $view_folder);
+	define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
 
 /*
- *------------------------------------------------------------------------------
+ * --------------------------------------------------------------------
  * LOAD THE BOOTSTRAP FILE
- *------------------------------------------------------------------------------
+ * --------------------------------------------------------------------
+ *
+ * And away we go...
  */
-require_once(BASEPATH . 'core/CodeIgniter.php');
+require_once BASEPATH.'core/CodeIgniter.php';
