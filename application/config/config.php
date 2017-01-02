c5de5d3fd80ac528e5676c03e3526533fa1a8918
Bob L <Bob L>
2017-01-02 13:38:58 -0600

    dynamic base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | localhost:8080 is the url when doing gulp serve.
 */
-$config['base_url'] = 'http://localhost:8080';
+//$config['base_url'] = 'http://localhost:8080';
+if (isset($_SERVER['HTTP_HOST']) && preg_match('/^((\[[0-9a-f:]+\])|(\d{1,3}(\.\d{1,3}){3})|[a-z0-9\-\.]+)(:\d+)?$/i', $_SERVER['HTTP_HOST']))
+{
+	$base_url = (is_https() ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST']
+		.substr($_SERVER['SCRIPT_NAME'], 0, strpos($_SERVER['SCRIPT_NAME'], basename($_SERVER['SCRIPT_FILENAME'])));
+}
+
+$config['base_url'] = $base_url;
 
 /*
 |--------------------------------------------------------------------------
