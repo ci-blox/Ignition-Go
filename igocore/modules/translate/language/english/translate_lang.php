b8737e4badd880fdce650863fe36a26e80fdf66d
Bob L <Bob L>
2017-05-29 23:14:12 -0500

    WIP translate module

---
+++ igocore/modules/translate/language/english/translate_lang.php
@@ -0,0 +1,32 @@
+<?php defined('BASEPATH') || exit('No direct script access allowed');
+
+$lang['translate_title_edit']  = '%s: Translate to %s';
+$lang['translate_title_index'] = 'Translate to %s';
+
+$lang['translate_no_modules'] = 'No Modules found with language files.';
+$lang['translate_no_core']    = 'No Core language files found.';
+
+$lang['translate_core']         = 'Core';
+$lang['translate_current_lang'] = 'Translate to';
+$lang['translate_modules']      = 'Modules';
+$lang['translate_language']     = 'Language';
+$lang['translate_other']        = 'Other...';
+$lang['translate_select']       = 'Select';
+$lang['translate_file']         = 'File';
+$lang['translate_translate']    = 'Translate';
+$lang['translate_new_lang']     = 'New Language';
+
+$lang['translate_export']       = 'Export Language Files';
+$lang['translate_export_short'] = 'Export';
+$lang['translate_export_note']  = 'Exporting language files provides you with a .zip file with all of the language files for that particular language so that you can easily share.';
+
+$lang['translate_include']      = 'Select Parts';
+$lang['translate_include_core'] = 'Include Core';
+$lang['translate_include_mods'] = 'Include Custom Modules';
+
+$lang['translate_save_success'] = 'Language file saved successfully.';
+$lang['translate_save_fail']    = 'There was an error trying to save language file. Check file permissions.';
+
+$lang['translate_success']      = '%s selected language entries were successfully translated.';
+$lang['translate_part_success'] = '%s selected language entries were successfully translated but %s failed.';
+$lang['translate_failed']       = 'The selected language entries were not translated. See the error log for details.';
