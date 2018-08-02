ab4b4df9183b3df468b61682c74bb93624b09832
Bob Lennes <boblennes@msn.com>
2018-08-01 22:17:40 -0500

    Update User Profile view and add fields

--- igocore/modules/users/controllers/Users.php
+++ igocore/modules/users/controllers/Users.php
@@ -49,6 +49,7 @@ class Users extends Front_Controller
 
         $this->load->config('countries');
         $this->load->helper('address');
+        $this->lang->load('usermaint');
 
         $this->load->config('user_meta');
         $meta_fields = config_item('user_meta_fields');
@@ -57,10 +58,10 @@ class Users extends Front_Controller
 
         if (isset($_POST['save'])) {
             $user_id = $this->current_user->id;
-            if ($this->saveUser('update', $user_id, $meta_fields)) {
+            if ($this->save_user('update', $user_id, $meta_fields)) {
                 $user = $this->user_model->find($user_id);
                 $log_name = empty($user->display_name) ?
-                    ($this->settings_lib->item('auth.use_usernames') ? $user->username : $user->email)
+                    ($this->siteSettings['auth.use_usernames'] ? $user->username : $user->email)
                     : $user->display_name;
 
                 // TODO log_activity
@@ -88,8 +89,8 @@ class Users extends Front_Controller
         // Generate password hint messages.
         $this->user_model->password_hints();
 
-        Template::set('user', $user);
-        Template::set('languages', array('' => '' ));// unserialize($this->settings_lib->item('site.languages')));
+        Template::set('record', $user);
+        Template::set('languages', array('' => '' ));// unserialize($this->siteSettings['site.languages')));
 
         Template::set_view('profile');
         Template::render();
@@ -203,4 +204,114 @@ class Users extends Front_Controller
         $this->db->set('password_hash',$pw_hash);
         return $this->db->update('users');
     }
+
+//--------------------------------------------------------------------
+
+	/**
+	 * Save the user
+	 *
+	 * @access private
+	 *
+	 * @param int   $id          The id of the user in the case of an edit operation
+	 * @param array $meta_fields Array of meta fields fur the user
+	 *
+	 * @return bool
+	 */
+	private function save_user($id=0, $meta_fields=array())
+	{
+
+		if ( $id == 0 )
+		{
+			$id = $this->current_user->id; /* ( $this->input->post('id') > 0 ) ? $this->input->post('id') :  */
+		}
+
+		$_POST['id'] = $id;
+
+		// Simple check to make the posted id is equal to the current user's id, minor security check
+		if ( $_POST['id'] != $this->current_user->id )
+		{
+			$this->form_validation->set_message('email', 'lang:us_invalid_userid');
+			return FALSE;
+		}
+
+		// Setting the payload for Events system.
+		$payload = array ( 'user_id' => $id, 'data' => $this->input->post() );
+
+        
+		$this->form_validation->set_rules('email', 'lang:us_email', 'required|trim|valid_email|max_length[120]|unique[users.email,users.id]');
+		$this->form_validation->set_rules('password', 'lang:us_password', 'max_length[120]|valid_password');
+
+		// check if a value has been entered for the password - if so then the pass_confirm is required
+		// if you don't set it as "required" the pass_confirm field could be left blank and the form validation would still pass
+		$extra_rules = !empty($_POST['password']) ? 'required|' : '';
+		$this->form_validation->set_rules('pass_confirm', 'lang:usermaint_password_confirm', ''.$extra_rules.'matches[password]');
+
+		$username_required = '';
+		if ($this->siteSettings['auth.login_type'] == 'username' ||
+		    $this->siteSettings['auth.use_usernames'])
+		{
+			$username_required = 'required|';
+		}
+		$this->form_validation->set_rules('username', 'lang:usermaint_username', $username_required . 'trim|max_length[30]|unique[users.username,users.id]');
+
+		$this->form_validation->set_rules('language', 'lang:usermaint_language', 'required|trim');
+		$this->form_validation->set_rules('timezone', 'lang:usermaint_timezone', 'required|trim|max_length[50]');
+		$this->form_validation->set_rules('display_name', 'lang:usermaint_display_name', 'trim|max_length[255]');
+
+		// Added Event "before_user_validation" to run before the form validation
+		Events::trigger('before_user_validation', $payload );
+
+
+        if (count($meta_fields)>0)
+       //TODO
+       /* foreach ($meta_fields as $field)
+		{
+			if ((!isset($field['admin_only']) || $field['admin_only'] === FALSE
+				|| (isset($field['admin_only']) && $field['admin_only'] === TRUE
+					&& isset($this->current_user) && $this->current_user->role_id == 1))
+				&& (!isset($field['frontend']) || $field['frontend'] === TRUE))
+			{
+				$this->form_validation->set_rules($field['name'], $field['label'], $field['rules']);
+			}
+		}
+*/
+
+		if ($this->form_validation->run() === FALSE)
+		{
+			return FALSE;
+		}
+
+		// Compile our core user elements to save.
+		$data = array(
+			'email'		=> $this->input->post('email'),
+			'language'	=> $this->input->post('language'),
+			'timezone'	=> $this->input->post('timezone'),
+		);
+
+		// If empty, the password will be left unchanged.
+		if ($this->input->post('password') !== '')
+		{
+			$data['password'] = $this->input->post('password');
+		}
+
+		if ($this->input->post('display_name') !== '')
+		{
+			$data['display_name'] = $this->input->post('display_name');
+		}
+
+		if (isset($_POST['username']))
+		{
+			$data['username'] = $this->input->post('username');
+		}
+
+		// Any modules needing to save data?
+		// Event to run after saving a user
+		Events::trigger('save_user', $payload );
+
+		return $this->user_model->update($id, $data);
+
+	}//end save_user()
+
+	
+
 }
--- igocore/modules/users/views/profile.php
+++ igocore/modules/users/views/profile.php
@@ -26,33 +26,160 @@ if (empty($renderPayload) && isset($this->auth)) {
     $renderPayload = $this->auth->user();
 }
 ?>
-<div class="col-sm-6 col-lg-4">
-<div class="form-group<?php echo form_error('username') ? ' error' : ''; ?>">
-    <?php echo form_label(lang('us_username'), 'username', array('class' => 'control-label')); ?>
-    <div class='controls'>
-        <input id='username' class='form-control' type='text' disabled="disabled" required='required' name='username' maxlength='30' value="<?php echo set_value('username', isset($user->username) ? $user->username : ''); ?>" />
+		<div class="col-md-offset-2 col-md-8">
+		    <div class="card">
+		        <div class="card-body">
+		            <div class="row">
+		                <div class="col-md-12">
+		                    <h4>Your Profile</h4>
+		                    <hr>
+		                </div>
+		            </div>
+		            <div class="row">
+		                <div class="col-md-12">
+		                    <form method="post">
+                                <input type="hidden" name='id' value='<?php echo $current_user->id; ?>'/>
+    <div class="form-group row<?php echo form_error('username') ? ' error' : ''; ?>">
+      <label for="username" class="col-4 col-form-label"><?php echo lang('usermaint_field_username');?></label> 
+      <div class="col-8">
+    <input type="text" maxlength='30' readonly="readonly"  id="username" name="username" placeholder="<?php echo lang('usermaint_field_username');?>" class="form-control" value="<?php echo set_value('username', isset($record->username) ? $record->username : ''); ?>"  /> 
         <span class='help-inline'><?php echo form_error('username'); ?></span>
+      </div>
     </div>
-</div>
-<div class="form-group<?php echo form_error('first_name') ? ' error' : ''; ?>">
-    <?php echo form_label(lang('us_first_name'), 'first_name', array('class' => 'control-label')); ?>
-    <div class='controls'>
-        <input id='first_name' class='form-control' type='text' disabled="disabled" required='required' name='first_name' maxlength='30' value="<?php echo set_value('first_name', isset($user->first_name) ? $user->first_name : ''); ?>" />
-        <span class='help-inline'><?php echo form_error('first_name'); ?></span>
+    <div class="form-group row<?php echo form_error('first_name') ? ' error' : ''; ?>">
+        <label for="first_name" class="col-4 col-form-label"><?php echo lang('usermaint_field_first_name') . '*';?></label> 
+        <div class="col-8">
+          <input type="text" maxlength='50'  id="first_name" name="first_name" placeholder="<?php echo lang('usermaint_field_first_name');?>" class="form-control"  required='required' value="<?php echo set_value('first_name', isset($record->first_name) ? $record->first_name : ''); ?>"  /> 
+          <span class='help-inline'><?php echo form_error('first_name'); ?></span>
+        </div>
+      </div>
+      <div class="form-group row<?php echo form_error('last_name') ? ' error' : ''; ?>">
+    <label for="last_name" class="col-4 col-form-label"><?php echo lang('usermaint_field_last_name') . '*';?></label> 
+    <div class="col-8">
+      <input type="text" maxlength='50'  id="last_name" name="last_name" placeholder="<?php echo lang('usermaint_field_last_name');?>" class="form-control"  required='required' value="<?php echo set_value('last_name', isset($record->last_name) ? $record->last_name : ''); ?>"  /> 
+      <span class='help-inline'><?php echo form_error('last_name'); ?></span>
     </div>
+  </div>
+  <div class="form-group row<?php echo form_error('email') ? ' error' : ''; ?>">
+  <label for="email" class="col-4 col-form-label"><?php echo lang('usermaint_field_email') . '*';?></label> 
+  <div class="col-8">
+    <input type="text" maxlength='254'  id="email" name="email" placeholder="<?php echo lang('usermaint_field_email');?>" class="form-control"  required='required' value="<?php echo set_value('email', isset($record->email) ? $record->email : ''); ?>"  /> 
+    <span class='help-inline'><?php echo form_error('email'); ?></span>
+  </div>
 </div>
-<div class="form-group<?php echo form_error('last_name') ? ' error' : ''; ?>">
-    <?php echo form_label(lang('us_last_name'), 'last_name', array('class' => 'control-label')); ?>
-    <div class='controls'>
-        <input id='last_name' class='form-control' type='text' disabled="disabled" required='required' name='last_name' maxlength='30' value="<?php echo set_value('last_name', isset($user->last_name) ? $user->last_name : ''); ?>" />
-        <span class='help-inline'><?php echo form_error('last_name'); ?></span>
+    <div class="form-group row<?php echo form_error('display_name') ? ' error' : ''; ?>">
+    <label for="display_name" class="col-4 col-form-label"><?php echo lang('usermaint_field_display_name');?></label> 
+    <div class="col-8">
+      <input type="text" maxlength='255'  id="display_name" name="display_name" placeholder="<?php echo lang('usermaint_field_display_name');?>" class="form-control" value="<?php echo set_value('display_name', isset($record->display_name) ? $record->display_name : ''); ?>"  /> 
+      <input type="hidden" maxlength='10'  id="display_name_changed" name="display_name_changed" placeholder="<?php echo lang('usermaint_field_display_name_changed');?>" class="form-control" value="<?php echo set_value('display_name_changed', isset($record->display_name_changed) ? $record->display_name_changed : ''); ?>"  /> 
+      <span class='help-inline'><?php echo form_error('display_name'); ?></span>
+      <span class='help-inline'><?php echo form_error('display_name_changed'); ?></span>
     </div>
-</div>
-<div class="form-group<?php echo form_error('timezone') ? ' error' : ''; ?>">
-    <?php echo form_label(lang('us_timezone'), 'timezone', array('class' => 'control-label')); ?>
-    <div class='controls'>
-        <input id='timezone' class='form-control' type='text' disabled="disabled" required='required' name='timezone' maxlength='30' value="<?php echo set_value('timezone', isset($user->timezone) ? $user->timezone : ''); ?>" />
-        <span class='help-inline'><?php echo form_error('timezone'); ?></span>
+  </div>
+
+
+  <?php
+  // Modified version of the timezone list function from http://stackoverflow.com/a/17355238/507629
+// Includes current time for each timezone (would help users who don't know what their timezone is)
+
+function generate_timezone_list() 
+{
+    static $regions = array(
+//        DateTimeZone::AFRICA,
+        DateTimeZone::AMERICA,
+//        DateTimeZone::ANTARCTICA,
+        DateTimeZone::ASIA,
+        DateTimeZone::ATLANTIC,
+        DateTimeZone::AUSTRALIA,
+        DateTimeZone::EUROPE,
+        DateTimeZone::INDIAN,
+        DateTimeZone::PACIFIC,
+    );
+
+    $timezones = array();
+    foreach( $regions as $region )
+    {
+        $timezones = array_merge( $timezones, DateTimeZone::listIdentifiers( $region ) );
+    }
+
+    $timezone_offsets = array();
+    foreach( $timezones as $timezone )
+    {
+        $tz = new DateTimeZone($timezone);
+        $timezone_offsets[$timezone] = $tz->getOffset(new DateTime);
+    }
+
+    // sort timezone by timezone name
+    ksort($timezone_offsets);
+
+    $timezone_list = array();
+    foreach( $timezone_offsets as $timezone => $offset )
+    {
+        $offset_prefix = $offset < 0 ? '-' : '+';
+        $offset_formatted = gmdate( 'H:i', abs($offset) );
+
+        $pretty_offset = "UTC${offset_prefix}${offset_formatted}";
+        
+        $t = new DateTimeZone($timezone);
+        $c = new DateTime(null, $t);
+        $current_time = $c->format('g:i A');
+
+        $timezone_list[$timezone] = "(${pretty_offset}) $timezone - $current_time";
+    }
+
+    return $timezone_list;
+}
+  ?>
+    <div class="form-group row<?php echo form_error('timezone') ? ' error' : ''; ?>">
+    <label for="timezone" class="col-4 col-form-label"><?php echo lang('usermaint_field_timezone');?></label> 
+    <div class="col-8">
+      <?php   
+                $options = generate_timezone_list();
+                echo form_dropdown(array('name' => 'timezone', 'class' => 'form-control' ), $options, set_value('timezone', isset($record->timezone) ? $record->timezone : 'America/Chicago'), lang('usermaint_field_timezone'));
+            ?> 
+          <span class='help-inline'><?php echo form_error('timezone'); ?></span>
     </div>
-</div>
+  </div>
+    <div class="form-group row<?php echo form_error('language') ? ' error' : ''; ?>">
+    <label for="language" class="col-4 col-form-label"><?php echo lang('usermaint_field_language');?></label> 
+    <div class="col-8">
+    <?php   
+                $options = array('english'=>'English',
+                'french'=>'French',
+                'italian'=>'Italian',
+                'russian'=>'Russian',
+                'spanish_am'=>'Spanish American'
+);
+                echo form_dropdown(array('name' => 'language', 'class' => 'form-control' ), $options, set_value('language', isset($record->language) ? $record->language : 'english'), lang('usermaint_field_language'));
+            ?> 
+      <span class='help-inline'><?php echo form_error('language'); ?></span>
+    </div>
+  </div>
+  <div class="form-group row<?php echo form_error('password') ? ' error' : ''; ?>">
+        <label for="password" class="col-4 col-form-label"><?php echo lang('usermaint_field_password');?></label> 
+        <div class="col-8">
+          <input type="text" maxlength='50'  id="password" name="password" placeholder="<?php echo lang('usermaint_field_password');?>" class="form-control" value="<?php echo set_value('password', isset($record->password) ? $record->password : ''); ?>"  /> 
+          <span class='help-inline'><?php echo form_error('password'); ?></span>
+        </div>
+      </div>
+      <div class="form-group row<?php echo form_error('pass_confirm') ? ' error' : ''; ?>">
+        <label for="pass_confirm" class="col-4 col-form-label"><?php echo lang('usermaint_field_password_confirm');?></label> 
+        <div class="col-8">
+          <input type="text" maxlength='50'  id="password_confirm" name="pass_confirm" placeholder="<?php echo lang('usermaint_field_password_confirm');?>" class="form-control" value="<?php echo set_value('pass_confirm', isset($record->password_confirm) ? $record->password_confirm : ''); ?>"  /> 
+          <span class='help-inline'><?php echo form_error('pass_confirm'); ?></span>
+        </div>
+      </div>
+                                    <div class="form-group row">
+                                <div class="offset-4 col-8">
+                                  <button name="save" type="submit" class="btn btn-primary">Update My Profile</button>
+                                </div>
+                              </div>
+                            </form>
+		                </div>
+		            </div>
+		            
+		        </div>
+		    </div>
+		</div>
+<div class="col-sm-6 col-lg-4">
 </div>
